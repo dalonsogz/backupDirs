faf96770a0f8705c8c098a0c25b31bf367432764
dalonsogz <dalonsogz@yahoo.es>
2021-12-31 18:10:48 +0100

    Last changes (time ago, wip GUI interface...)

--- backupDirs.py
+++ backupDirs.py
@@ -1,27 +1,30 @@
-#!/usr/bin/env python3
+import logging
+import logging.config
+import yaml
+# !/usr/bin/env python3
 
 import sys, shutil, readchar, traceback
 import hashlib, os
-from pathlib import PurePath
+from pathlib import PurePath, Path
 from itertools import filterfalse
 
 
 def printExceptionDetails(inst, message, object):
     if message:
-        print("Error processing '{}':".format(message, object))
+        logger.error("Error processing '{}':".format(message, object))
 
-    print(type(inst))  # the exception instance
-    print(inst.args)  # arguments stored in .args
-    print(inst)  # __str__ allows args to be printed directly, but may be overridden in exception subclasses
+    logger.error(type(inst))  # the exception instance
+    logger.error(inst.args)  # arguments stored in .args
+    logger.error(inst)  # __str__ allows args to be printed directly, but may be overridden in exception subclasses
 
 
 def writeToMD5File(md5File, md5hashes):
     with open(md5File, "a+") as afile:
         for md5hash in md5hashes:
-            afile.write("{} *.{}\n".format(md5hash[0], md5hash[1]))
+            afile.write("{} *.\\{}\n".format(md5hash[0], md5hash[1]))
 
 
-def getHashofDirs(directory, verbose=0):
+def getHashofDirs(directory, verbose=0, silent=0):
     BLOCKSIZE = 65536
     hasher = hashlib.md5()
     if not os.path.exists(directory):
@@ -33,7 +36,7 @@ def getHashofDirs(directory, verbose=0):
             for fileName in files:
                 hasherFile = hashlib.md5()
                 if verbose > 1:
-                    print("Hashing: '{}'".format(root + os.path.sep + fileName))
+                    logger.debug("Hashing: '{}'".format(root + os.path.sep + fileName))
                 filepath = os.path.join(root, fileName)
                 with open(filepath, "rb") as afile:
                     buf = afile.read(BLOCKSIZE)
@@ -49,12 +52,12 @@ def getHashofDirs(directory, verbose=0):
         return -2
 
     if verbose > 0:
-        print("Hash '{}':'{}'".format(directory, hasher.hexdigest()))
+        logger.debug("Hash '{}':'{}'".format(directory, hasher.hexdigest()))
 
     return hasher.hexdigest(), md5hashes
 
 
-def removeOldTargets(baseSourceFile, baseTargetPath, verbose=0):
+def removeOldTargets(baseSourceFile, baseTargetPath, verbose=0, silent=0):
     selectedDirs = []
     with open(baseSourceFile, 'r') as f:
         item = f.read()
@@ -67,25 +70,29 @@ def removeOldTargets(baseSourceFile, baseTargetPath, verbose=0):
 
     origSelDirs = selDirs.copy()
 
-    # print("selDirs: '{}'".format(selDirs))
+    # logger.debug("selDirs: '{}'".format(selDirs))
 
     oldTargetsDeleted = []
     oldTargetsNotDeleted = []
     oldTargetsNotFound = selDirs.copy()
     # Remove listed directories in file
     ########################################################
-    for item in baseTargetPath.iterdir():
+    baseTargetPathConcrete = Path(baseTargetPath)
+    for item in baseTargetPathConcrete.iterdir():
         if verbose > 1:
-            print("Item.name: '{}', ItemType: '{}'".format(item.name, type(item)))
+            logger.debug("Item.name: '{}', ItemType: '{}'".format(item.name, type(item)))
         if item.is_dir() and item.name in selDirs:
             try:
                 oldTargetsNotFound.remove(item.name)
                 userResponse = ''
                 while userResponse != 'S' and userResponse != 'N' and userResponse != 'L':
-                    print("Do you want to remove '{}' (S/N)?".format(item.name))
-                    userResponse = readchar.readkey().upper()
+                    logger.info("Do you want to remove '{}' (S/N)?".format(item.name))
+                    if silent == 0:
+                        userResponse = readchar.readkey().upper()
+                    else:
+                        userResponse = 'S'
                 if userResponse.capitalize() == 'S':
-                    print("DirDeletion: '{}'".format(item.name.strip()))
+                    logger.info("DirDeletion: '{}'".format(item.name.strip()))
                     #            shutil.move(item, "{} (removed)".format(str(item).strip()))
                     shutil.rmtree(item)
                     selDirs.remove(item.name)
@@ -93,72 +100,96 @@ def removeOldTargets(baseSourceFile, baseTargetPath, verbose=0):
             except Exception as inst:
                 printExceptionDetails(inst, "Error removing", item)
 
-    print("Complete list:{}".format(origSelDirs))
-    print("Not found:{}".format(selDirs))
+    logger.info("Complete list:{}".format(origSelDirs))
+    logger.info("Not found:{}".format(selDirs))
     oldTargetsNotDeleted = list(filterfalse(oldTargetsNotFound.__contains__, selDirs))
 
     return oldTargetsDeleted, oldTargetsNotDeleted, oldTargetsNotFound
 
 
-def copyNewTargets(baseSourcePath, targetPath, targetMD5File, verbose=0):
+def copyNewTargets(baseSourcePath, targetPath, targetMD5File, verbose=0, silent=0):
     deletedSources = []
     copiedSources = []
     copyFailed = []
     notCopied = []
     # Copy directories from source path to target path
     ########################################################
-    for item in baseSourcePath.iterdir():
-        #  print("ItemType: '{}'".format(type(item)))
+    baseSourcePathConcrete = Path(baseSourcePath)
+    for item in baseSourcePathConcrete.iterdir():
+        #  logger.debug("ItemType: '{}'".format(type(item)))
         if item.is_dir():
             try:
                 userResponse = ''
                 while userResponse != 'S' and userResponse != 'N' and userResponse != 'L':
-                    print("Do you want to copy '{}' (S/N)?".format(item.name))
-                    userResponse = readchar.readkey().upper()
+                    logger.info("Do you want to copy '{}' (S/N)?".format(item.name))
+                    if silent == 0:
+                        userResponse = readchar.readkey().upper()
+                    else:
+                        userResponse = 'S'
                 if userResponse.capitalize() == 'S':
-                    print("Calculating source hash...")
+                    logger.info("Calculating source hash...")
                     hashSource, md5hashes = getHashofDirs(item, verbose)
-                    print("Copying: '{}'".format(item.name.strip()))
-                    shutil.copytree(item,PurePath(targetPath, item.name))
-                    print("Calculating target hash...")
+                    logger.info("Copying: '{}'".format(item.name.strip()))
+                    shutil.copytree(item, PurePath(targetPath, item.name))
+                    logger.info("Calculating target hash...")
                     hashTarget, md5hashes = getHashofDirs(PurePath(targetPath, item.name), verbose)
-                    print("hashSource={},hashTarget={}".format(hashSource, hashTarget))
+                    logger.debug("hashSource={},hashTarget={}".format(hashSource, hashTarget))
                     copiedSources.append(item.name)
                     if hashSource == hashTarget:
                         writeToMD5File(targetMD5File, md5hashes)
-                        print("Hash copy matches, proceding to delete source directory '{}' (S/N)".format(item))
-                        userResponse = readchar.readkey().upper()
+                        logger.info("Hash copy matches, proceding to delete source directory '{}' (S/N)".format(item))
+                        if silent == 0:
+                            userResponse = readchar.readkey().upper()
+                        else:
+                            userResponse = 'S'
                         if userResponse.capitalize() == 'S':
                             shutil.rmtree(item)
                             deletedSources.append(item.name)
-                            print("Deleted source directory '{}'".format(item))
+                            logger.info("Deleted source directory '{}'".format(item))
                     else:
                         copyFailed(item.name)
-                        print("Hash copy doesn't match the source")
+                        logger.error("Hash copy doesn't match the source")
                 else:
                     notCopied.append(item.name)
-                    print("Not copied '{}'".format(item.name))
+                    logger.info("Not copied '{}'".format(item.name))
             except Exception as inst:
                 printExceptionDetails(inst, "Error copying", item)
-                print("inst:{}".format(inst))
+                logger.error("inst:{}".format(inst))
                 copyFailed.append((item.name, inst.args[0]))
 
     return deletedSources, copiedSources, copiedSources, notCopied, copyFailed
 
 
+def init_logger():
+    with open('config.yml', 'r') as f:
+        config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+    return logging.getLogger()  # "web_scraper")
+
+
+global logger
+
+
 def main(argv):
-    verbose = 0
+    global logger
+    logger = init_logger()
+
+    verbose = 2
+    silent = 1
     sourceFile = "notas.txt"
     targetMD5File = "checksums.md5"
 
-    # Pruebas
-    sourcePath = "_source"
-    targetPathBase = "E:\prog\python\pruebas"
-    targetPath = "_target"
+    if len(sys.argv) == 4:
+        sourcePath = sys.argv[1]
+        targetPathBase = sys.argv[2]
+        targetPath = sys.argv[3]
+    else:
+        sourcePath = "_source"
+        targetPathBase = "E:\prog\python\pruebas"
+        targetPath = "juegos"
 
-    # sourcePath = "D:\_juegos\__nuevo__\_J4"
-    # targetPathBase = "P:\\"
-    # targetPath = "juegos"
+    logger.warning("sourcePath={},targetPathBase={},targetPath={}".format(sourcePath, targetPathBase, targetPath))
+
     baseTargetPath = PurePath(targetPathBase, targetPath)
     baseTargetMD5File = PurePath(targetPathBase, targetMD5File)
     baseSourcePath = PurePath(sourcePath)
@@ -172,9 +203,10 @@ def main(argv):
     # deletedSources, copiedSources, copiedSources, notCopied, copyFailed = "","","","",""
 
     oldTargetsDeleted, oldTargetsNotDeleted, oldTargetsNotFound = removeOldTargets(baseSourceFile, baseTargetPath,
-                                                                                   verbose)
+                                                                                   verbose, silent)
     deletedSources, copiedSources, copiedSources, notCopied, copyFailed = copyNewTargets(baseSourcePath, baseTargetPath,
-                                                                                         baseTargetMD5File, verbose)
+                                                                                         baseTargetMD5File, verbose,
+                                                                                         silent)
 
     print("----------------------------------------------------------------------------------------------------")
     print("oldTargetsNotFound:\n{}".format(oldTargetsNotFound))
